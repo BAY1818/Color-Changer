dbc196ee54c3e386b4c6e48a500d444264f1b2ae
Baygeldi <baygeldi0718@gmail.com>
2025-09-17 21:56:23 +0800

    Paragraph added

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function ColorPicker(){
+
+    const [color, setColor] = useState("#FFFFFF");    
+
+    return (<div className="color-picker-container">
+        <h1>Color Changer</h1>
+        <div className='color-display' style={{backgroundColor: color}}>
+            <p>Selected Color: {color}</p>
+        </div>
+    </div>);
+}
+
+export default ColorPicker
