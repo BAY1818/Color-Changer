db294e5d84fc9143a64123ec71bc87f9f8f40cb0
Baygeldi <baygeldi0718@gmail.com>
2025-09-24 14:11:44 +0800

    Done

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -13,7 +13,7 @@ function ColorPicker(){
         <div className='color-display' style={{backgroundColor: color}}>
             <p>Selected Color: {color}</p>
         </div>
-        <label>Select a Color:</label>
+        <label>Select a Color:  </label>
         <input type="color" value={color} onChange={handleColorChange} />
     </div>);
 }
